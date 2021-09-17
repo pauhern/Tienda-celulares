0200c104b22264d701505f22aa6cad402fc7c559
pauhern <90876304+pauhern@users.noreply.github.com>
2021-09-16 23:09:10 -0600

    Agregando frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("Si lo puedes soñar, lo puedes lograr.");
         }
     }
 }
